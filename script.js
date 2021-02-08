f86038a2d7b856fb7f8816d66e88ea00ef405bb0
marleigh <volcana@gmail.com>
2021-02-08 09:49:28 -0800

    some basic fetch additions

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const btn = document.getElementById('btn');
-const form = document.getElementsById('generate');
+const form = document.getElementById('generate');
+const url = 'https://foodish-api.herokuapp.com/api/';
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    generateFood();
+});
+
+async function generateFood() {
+    let promise = await fetch(url);
+    let json = promise.json();
+
+    console.log(json);
+    //display image
+
+    //is there more info to go with the pictures? nope
+
+    //potentially hide old button / make new one? 
+}
