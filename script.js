96bed1d846f1fe333b9748dfcfd00d9d29005488
marleigh <volcana@gmail.com>
2021-02-08 13:35:42 -0800

    having fun with fetch

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const btn = document.getElementById('btn');
+const randomBtn = document.getElementById('random-btn');
+const pizzaBtn = document.getElementById('pizza-btn');
+const burgerBtn = document.getElementById('burger-btn');
+const biryaniBtn = document.getElementById('biryani-btn');
+
+
 const form = document.getElementById('generate');
+const imageDiv = document.querySelector(".image");
+let image = document.createElement('img');
+
 const url = 'https://foodish-api.herokuapp.com/api/';
 
-btn.addEventListener('click', (e) => {
+async function fetchCall(param) {
+    let promise = await fetch(url + param);
+    let json = await promise.json();
+
+    image.src = json.image;
+    image.classList.add('responsive');
+    imageDiv.appendChild(image);
+}
+
+fetchCall('images/pizza');
+
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    generateFood();
+    //need to grab which button was clicked
+    console.log(form)
+    //fetchCall();
 });
-
-async function generateFood() {
-    let promise = await fetch(url);
-    let json = promise.json();
-
-    console.log(json);
-    //display image
-
-    //is there more info to go with the pictures? nope
-
-    //potentially hide old button / make new one? 
-}
